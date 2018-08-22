79fb64e5bf73a78959e190dcb1277f45ad64e6b7
yijiull <1147161372@qq.com>
2018-08-22 20:27:56 +0800

    AC自动机；hiho1036

--- ac.cpp
+++ ac.cpp
@@ -6,12 +6,14 @@ void AC::insert(const std::string &s, int v){
         int c = idx(s[i]);
         if(!ch[u][c]){ //new node
             memset(ch[sz], 0, sizeof( ch[sz]));
-            val[sz] = 0;
+            //val[sz] = 0;
+            match[sz] = 0;
             ch[u][c] = sz++;
         } 
         u = ch[u][c];
     }
-    val[u] = v;
+    //val[u] = v;
+    match[u] = 1;
 }
 void AC::getFail(){
     std::queue<int> q;
@@ -21,7 +23,7 @@ void AC::getFail(){
         if(u){
             q.push(u);
             f[u] = 0;
-            last[u] = 0;
+            //last[u] = 0;
         }
     }
     while(!q.empty()){
@@ -29,29 +31,57 @@ void AC::getFail(){
         q.pop();
         for(int c = 0; c < sigma; c++){
             int u =ch[r][c];
-            if(!u) continue;
+            if(!u) {
+                ch[r][c] = ch[f[r]][c];    
+                continue;
+            }
             q.push(u);
             int v = f[r];
             while(v && !ch[v][c]) v = f[v];
             f[u] = ch[v][c];
-            last[u] = val[f[u]] ? f[u] : last[f[u]];
+            //last[u] = val[f[u]] ? f[u] : last[f[u]];
+            match[u] |= match[f[u]];
         }
     }
 }
 void AC::print(int u){
     if(u){
-        cnt[val[u]]++;
-        print(last[u]);
+       // cnt[val[u]]++;
+       // print(last[u]);
     }
 }
-void AC::find(const std::string &s){
+
+int AC::find(const std::string &s){
     int n = s.length();
     int u = 0;
     for(int i = 0; i < n; i++){
         int c = idx(s[i]);
         while(u && !ch[u][c]) u = f[u];
         u = ch[u][c];
-        if(val[u]) print(u);
-        else if(last[u]) print(last[u]);
+        if(match[u]) return 1;
+        //if(val[u]) print(u);
+        //else if(last[u]) print(last[u]);
+    }
+    return 0;
+}
+
+//hiho1036trie图
+AC ac;
+#include <iostream>
+using namespace std;
+int main(){
+    string s, p;
+    int n;
+    cin>>n;
+    while(n--){
+        cin>>s;
+        ac.insert(s, 0);
+    }
+    ac.getFail();
+    cin>>p;
+    if(ac.find(p)){
+        cout<<"YES\n";
+    }else{
+        cout<<"NO\n";
     }
 }
--- ac.h
+++ ac.h
@@ -9,14 +9,16 @@ const int sigma = 26; //字符集大小
 class AC{
 private:
     int ch[maxnode][sigma];
-    int last[maxnode], val[maxnode], cnt[maxnode];
+    //int last[maxnode], val[maxnode], cnt[maxnode];
+    int match[maxnode];
     int f[maxnode];
     int sz;
 
 public:
     AC(){
         memset(ch[0], 0, sizeof( ch[0]));
-        memset(cnt, 0, sizeof( cnt));
+        //memset(cnt, 0, sizeof( cnt));
+        memset(match, 0, sizeof match);
         sz = 1;
     }
     int idx(char c){
@@ -25,6 +27,6 @@ public:
     void insert(const std::string &s, int v);
     void getFail();
     void print(int u);
-    void find(const std::string &s);
+    int find(const std::string &s);
 
 };
